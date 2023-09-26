f74b245afec428095aec9f1a0001b113d597bffb
Alisakono <alisa.konovalov@web.de>
2023-09-26 13:23:12 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello,");
+        System.out.println("my");
+        System.out.println("name");
+        System.out.println("is");
+        System.out.println("Alisa");
+
+        System.out.print("Hello,");
+        System.out.print("my");
+        System.out.print(" name");
+        System.out.print(" is");
+        System.out.print(" Alisa.");
+    }
+}
+/*    В методе main добавить инструкцию System.out.println(); необходимое количество раз, чтобы в консоль распечатался текст:
+        Hello,
+        my
+        name
+        is
+<your name>!
+        Используя инструкцию System.out.print(); распечатайте в консоль текст:
+        Hello, my name is <your name>!*/
+
+
+
+
+
+
+
+
+
+
